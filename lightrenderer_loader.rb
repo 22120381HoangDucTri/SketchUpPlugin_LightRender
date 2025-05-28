d698c0cb0f8b4f74bf4d13b9f3f3bda1c1db357c
Hoàng Đức Trí <119548525+22120381HoangDucTri@users.noreply.github.com>
2025-05-28 19:46:46 +0700

    Add files via upload

---
+++ lightrenderer_loader.rb
@@ -0,0 +1,27 @@
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module LightRenderer
+  unless file_loaded?(__FILE__)
+    ex = SketchupExtension.new('My Cube', 'LightRenderer/sub')
+    ex.description = 'Render Light'
+    ex.version     = '1.0.0'
+    ex.copyright   = 'Tri & Tri 2025'
+    ex.creator     = 'Tri'
+
+    Sketchup.register_extension(ex, true)
+    file_loaded(__FILE__)
+  end
+
+  def self.reload
+    original_verbose = $VERBOSE
+    $VERBOSE = nil
+    pattern = File.join(__dir__, '**/*.rb')
+
+    Dir.glob(pattern).each { |file|
+      load file
+    }.size
+  ensure
+    $VERBOSE = original_verbose
+  end
+end
